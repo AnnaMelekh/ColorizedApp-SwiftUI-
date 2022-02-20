7d112381e9e185d81c4cc8b4444caebebfd3aeaf
AnnaMelekh <annasmelekhina@gmail.com>
2022-02-20 16:57:51 +0300

    Initial Commit

---
+++ ColorizedApp (SwiftUI)/ColorizedApp__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp__SwiftUI_App.swift
+//  ColorizedApp (SwiftUI)
+//
+//  Created by Anna Melekhina on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp (SwiftUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedApp (SwiftUI)
+//
+//  Created by Anna Melekhina on 20.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
