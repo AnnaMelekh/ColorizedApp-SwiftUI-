09e026334dd8436b9737b9f50055b2c2cade629c
AnnaMelekh <annasmelekhina@gmail.com>
2022-02-20 18:43:12 +0300

    created Rectangle, Slider. Text.TextField

--- ColorizedApp (SwiftUI)/ContentView.swift
+++ ColorizedApp (SwiftUI)/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderRedValue = Double.random(in: 0...255)
+    @State private var sliderRedText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Color(#colorLiteral(red: 0.1156305, green: 0.4357837439, blue: 0.7079777122, alpha: 1))
+                .ignoresSafeArea()
+            VStack{
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundColor(.red)
+                    .frame(width: 250, height: 100)
+                HStack{
+                    Text("\(sliderRedValue)")
+                        .padding()
+                    Slider(value: $sliderRedValue)
+                        .padding()
+                    TextField("text", text: $sliderRedText)
+                        .background()
+                        .padding()
+                }
+            }
+        }
     }
 }
 
